c5f94bcd19e5e2afe90aee1e8aaf686ac216c60f
Jason <beefers331@gmail.com>
2022-02-05 03:05:26 -0700

    Added delete for textures

--- src/truss3D/textures.nim
+++ src/truss3D/textures.nim
@@ -41,6 +41,9 @@ const
 proc genTexture*(): Texture =
   glCreateTextures(GlTexture2D, 1, result.Gluint.addr)
 
+proc delete*(tex: var Texture) =
+  glDeleteTextures(1, tex.Gluint.addr)
+
 proc copyTo*(img: Image, tex: Texture) =
   glTextureStorage2D(tex.Gluint, 1.GlSizei, GlRgba8, img.width.GlSizei, img.height.GlSizei)
   glTextureSubImage2D(tex.Gluint,
