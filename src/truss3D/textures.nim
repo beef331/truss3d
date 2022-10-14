5ca3eafcdc3d769f25a6555efc214a2bed7d0127
jason <beefers331@gmail.com>
2022-10-14 01:19:35 -0600

    Fixed various bugs with framebuffers

--- src/truss3D/textures.nim
+++ src/truss3D/textures.nim
@@ -10,16 +10,18 @@ type
   Texture* = distinct Gluint
   RenderBuffer* = distinct GLuint
   FrameBufferId* = distinct Gluint
+
+  FrameBufferKind* = enum
+    Color, Depth
+
   FrameBuffer* = object
     id: FrameBufferId
     size*: IVec2
     format: TextureFormat
+    textures: set[FrameBufferKind]
     colourTexture*: Texture
     clearColor*: Color
-    case hasDepth*: bool
-    of true:
-      depthTexture*: Texture
-    else: discard
+    depthTexture*: Texture
 
 
 const 
@@ -56,6 +58,16 @@ proc copyTo*(img: Image, tex: Texture) =
                       GlUnsignedByte,
                       img.data[0].unsafeAddr)
 
+template with*(fb: FrameBuffer, body: untyped) =
+  block:
+    var currentBuffer: Gluint
+    glGetIntegerv(GlDrawFrameBufferBinding, cast[ptr Glint](addr Gluint(currentBuffer)))
+    fb.bindBuffer()
+    body
+    glBindFrameBuffer(GlFrameBuffer, currentBuffer)
+    #glClear(GLDepthbufferBit or GlColorBufferBit)
+
+
 proc bindBuffer*(fb: FrameBuffer) =
   glBindFrameBuffer(GlFrameBuffer, fb.id.Gluint)
 
@@ -63,39 +75,41 @@ proc unbindFrameBuffer*() =
   glBindFrameBuffer(GlFrameBuffer, 0.Gluint)
 
 proc attachTexture*(buffer: var FrameBuffer) =
-  glBindTexture(GlTexture2d, buffer.colourTexture.Gluint)
-  glTexImage2D(GlTexture2d, 0.Glint, formatLut[buffer.format].Glint, buffer.size.x.GlSizei, buffer.size.y.GlSizei, 0.Glint, formatLut[buffer.format], dataType[buffer.format], nil)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-  buffer.bindBuffer()
-  glFramebufferTexture2D(GlFrameBuffer, GlColorAttachment0, GlTexture2D, buffer.colourTexture.Gluint, 0)
+  with buffer:
+    if Color in buffer.textures:
+      glBindTexture(GlTexture2d, buffer.colourTexture.Gluint)
+      glTexImage2D(GlTexture2d, 0.Glint, formatLut[buffer.format].Glint, buffer.size.x.GlSizei, buffer.size.y.GlSizei, 0.Glint, formatLut[buffer.format], dataType[buffer.format], nil)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+      glFramebufferTexture2D(GlFrameBuffer, GlColorAttachment0, GlTexture2D, buffer.colourTexture.Gluint, 0)
 
-  if buffer.hasDepth:
-    glBindTexture(GlTexture2d, buffer.depthTexture.Gluint)
-    glTexImage2D(GlTexture2d, 0.Glint, GlDepthComponent.Glint, buffer.size.x.GlSizei, buffer.size.y.GlSizei, 0.Glint, GlDepthComponent, cGlFloat, nil)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glFramebufferTexture2D(GlFrameBuffer, GlDepthAttachment, GlTexture2D, buffer.depthTexture.Gluint, 0)
+    if Depth in buffer.textures:
+      glBindTexture(GlTexture2d, buffer.depthTexture.Gluint)
+      glTexImage2D(GlTexture2d, 0.Glint, GlDepthComponent.Glint, buffer.size.x.GlSizei, buffer.size.y.GlSizei, 0.Glint, GlDepthComponent, cGlFloat, nil)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+      glFramebufferTexture2D(GlFrameBuffer, GlDepthAttachment, GlTexture2D, buffer.depthTexture.Gluint, 0)
 
-  unbindFrameBuffer()
-  glBindTexture(GlTexture2d, Gluint(0))
+    glBindTexture(GlTexture2d, Gluint(0))
 
 proc clear*(fb: FrameBuffer) =
-  fb.bindBuffer()
-  let colorPtr = fb.clearColor.r.unsafeAddr
-  glClearBufferfv(GlColor, 0, colorPtr)
-  if fb.hasDepth:
-    let depth: GlFloat = 1
-    glClearBufferfv(GlDepth, 0, depth.unsafeaddr)
+  with fb:
+    if Color in fb.textures:
+      let colorPtr = fb.clearColor.r.unsafeAddr
+      glClearBufferfv(GlColor, 0, colorPtr)
+    if Depth in fb.textures:
+      let depth: GlFloat = 1
+      glClearBufferfv(GlDepth, 0, depth.unsafeaddr)
 
-proc genFrameBuffer*(size: Ivec2, format: TextureFormat, hasDepth = true): FrameBuffer =
-  result = FrameBuffer(size: size, hasDepth: hasDepth)
+proc genFrameBuffer*(size: Ivec2, format: TextureFormat, textures = {FrameBufferKind.Color, Depth}): FrameBuffer =
+  assert textures != {}
+  result = FrameBuffer(size: size, textures: textures)
   result.colourTexture = genTexture()
-  if result.hasDepth:
+  if Depth in result.textures:
     result.depthTexture = genTexture()
   glCreateFramebuffers(1, result.id.Gluint.addr)
   result.attachTexture()
@@ -105,12 +119,3 @@ proc resize*(fb: var FrameBuffer, size: IVec2) =
   if size != fb.size:
     fb.size = size
     fb.attachTexture()
-
-template with*(fb: FrameBuffer, body: untyped) = 
-  block:
-    var currentBuffer: FrameBuffer
-    glGetIntegerv(GlDrawFrameBuffer, cast[ptr Glint](addr Gluint(currentBuffer.id)))
-    fb.bindBuffer()
-    body
-    currentBuffer.bindBuffer()
-    #glClear(GLDepthbufferBit or GlColorBufferBit)
