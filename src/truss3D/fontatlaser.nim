218d5a4d0ac0bd41cb938cc5d090f62af4943f2a
Jason Beetham <beefers331@gmail.com>
2024-05-11 13:39:40 -0600

    Optimised font atlasser a bit by not double hashing

--- src/truss3D/fontatlaser.nim
+++ src/truss3D/fontatlaser.nim
@@ -18,7 +18,7 @@ type
     entries*: Table[Rune, FontEntry]
     font*: Font
     rectData: seq[Rect]
-    ssbo*: Ssbo[seq[Rect]] 
+    ssbo*: Ssbo[seq[Rect]]
 
 proc `=dup`(atlas: FontAtlas): FontAtlas {.error.}
 
@@ -42,7 +42,7 @@ proc setFont*(atlas: var FontAtlas, font: Font) =
   atlas.rectData.setLen(0)
   atlas.texture.clearBlack()
 
-proc blit(atlas: var FontAtlas, rune: Rune, runeStr: string, image: Image, size: Vec2) =
+proc blit(atlas: var FontAtlas, rune: Rune, runeStr: string, image: Image, size: Vec2): FontEntry =
   let (added, rect) = atlas.atlas.add(rune, Rect.init(size.x, size.y))
   if added:
     atlas.font.paint = rgb(255, 255, 255)
@@ -55,30 +55,34 @@ proc blit(atlas: var FontAtlas, rune: Rune, runeStr: string, image: Image, size:
       Gluint atlas.texture, GlTexture2d, 0, Glint rect.x, Glint rect.y, 0,
       GlSizei size.x, GlSizei size.y, 1
     )
-    tex.delete()   
+    tex.delete()
 
     info "Added: '", runeStr, "' to atlas, at: ", rect
 
     atlas.rectData.add rect
     atlas.rectData.copyTo(atlas.ssbo)
-    atlas.entries[rune] = FontEntry(id: atlas.rectData.len, rect: rect, rune: rune)  
+    result = FontEntry(id: atlas.rectData.len, rect: rect, rune: rune)
+    atlas.entries[rune] = result
+
 
   else:
     error "Did not add: '", runeStr, "'to atlas."
 
-proc blit(atlas: var FontAtlas, rune: Rune) =
-  if rune notin atlas.entries:
+proc blit(atlas: var FontAtlas, rune: Rune): FontEntry =
+  atlas.entries.withValue rune, entry:
+    result = entry[]
+  do:
     let
       runeStr = $rune
       size = atlas.font.layoutBounds(runeStr)
       image = newImage(int size.x, int size.y)
 
-    atlas.blit(rune, runeStr, image, size)
+    result = atlas.blit(rune, runeStr, image, size)
+
 
 proc runeEntry*(atlas: var FontAtlas, rune: Rune): FontEntry =
   if rune.isWhiteSpace:
     FontEntry()
   else:
     atlas.blit(rune)
-    atlas.entries[rune]
 
