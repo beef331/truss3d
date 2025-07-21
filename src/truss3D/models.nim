b6a1f4816762be871d7b10290e2b8e186f9b497d
Jason Beetham <beefers331@gmail.com>
2025-07-21 14:16:41 -0600

    If there is no model scene assume it's a single model

--- src/truss3D/models.nim
+++ src/truss3D/models.nim
@@ -41,89 +41,104 @@ when useAssimp:
         error path, " invalid model file."
         raise newException(IOError, path & " invalid model file")
 
+    var meshInds: seq[(cint, TMatrix4x4)]
     for child in scene.rootNode.ichildren:
       for meshInd in child.meshes.toOpenArray(0, child.meshCount - 1):
-        let mesh = scene.meshes[meshInd]
-        type VboKinds = enum
-          vert, norm, uv, col
-        const vboSize: array[VboKinds, GLsizei] = [Glsizei sizeof TVector3d, Glsizei sizeof TVector3d, Glsizei sizeof TVector3d, Glsizei sizeof TColor4d]
-        var vbos: array[VboKinds, Gluint]
-        let
-          components = block:
-            var comps = {vert}
-            if mesh.hasNormals():
-              comps.incl norm
-            if mesh.hasUvs():
-              comps.incl uv
-            if mesh.hasColors():
-              comps.incl col
-            comps
+        meshInds.add (meshInd, child.transformation)
 
-        let mat = child.transformation
-        for vertInd in 0..<mesh.vertexCount:
-          let
-            vert = cast[Vec3](mesh.vertices[vertInd]).vec4(1)
-
-            tsfm = mat4(
-              vec4(mat[0], mat[4], mat[8], mat[12]),
-              vec4(mat[1], mat[5], mat[9], mat[13]),
-              vec4(mat[2], mat[6], mat[10], mat[14]),
-              vec4(mat[3], mat[7], mat[11], mat[15])
-            )
-
-          mesh.vertices[vertInd] = cast[TVector3D]((tsfm * vert).xyz)
-
-        glCreateBuffers(ord(VboKinds.high) + 1, vbos[vert].addr)
-        glNamedBufferStorage(vbos[vert], mesh.vertexCount * vboSize[vert], mesh.vertices, GLbitfield 0)
-
-        if norm in components:
-          glNamedBufferStorage(vbos[norm], mesh.vertexCount * vboSize[norm], mesh.normals, GLbitfield 0)
-
-        if uv in components:
-          glNamedBufferStorage(vbos[uv], mesh.vertexCount * vboSize[uv], mesh.texCoords[0], GLbitfield 0)
-
-        if col in components:
-          glNamedBufferStorage(vbos[col], mesh.vertexCount * vboSize[col], mesh.colors[0], GLbitfield 0)
-
-        var msh: Mesh
-        glCreateVertexArrays(1, msh.vao.addr)
-        glCreateBuffers(1, msh.indices.addr)
-
-        var
-          indices = newSeqOfCap[cint](mesh.faceCount * 3)
-
-        for face in mesh.ifaces:
-          if face.indexCount != 3:
-            raise (ref ValueError)(msg: "Only acceepting triangulated meshes")
-          indices.add cast[ptr array[3, cint]](face.indices)[]
-
-        msh.size = indices.len.GlSizei
-
-        glNamedBufferStorage(msh.indices,
-          msh.size * sizeof(cint),
-          indices[0].addr,
-          GLbitfield 0
+    if meshInds.len == 0: # In the case we don't find a tree, assume it's just an empty model file
+      for x in 0..<scene.meshCount:
+        meshInds.add (x,
+          [
+            1f, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+          ]
         )
 
-        glVertexArrayElementBuffer(msh.vao, msh.indices)
+    for (meshInd, transform) in meshInds:
+      let mesh = scene.meshes[meshInd]
+      type VboKinds = enum
+        vert, norm, uv, col
+      const vboSize: array[VboKinds, GLsizei] = [Glsizei sizeof TVector3d, Glsizei sizeof TVector3d, Glsizei sizeof TVector3d, Glsizei sizeof TColor4d]
+      var vbos: array[VboKinds, Gluint]
+      let
+        components = block:
+          var comps = {vert}
+          if mesh.hasNormals():
+            comps.incl norm
+          if mesh.hasUvs():
+            comps.incl uv
+          if mesh.hasColors():
+            comps.incl col
+          comps
 
-        for ind, vbo in vbos.pairs:
-          if ind in components:
-            glVertexArrayVertexBuffer(msh.vao, Gluint ind, vbo, 0, vboSize[ind])
-            glEnableVertexArrayAttrib(msh.vao, Gluint ind)
-            case ind
-            of vert:
-              glVertexArrayAttribFormat(msh.vao, Gluint vert, 3, cGlFloat, GlFalse, 0)
-            of norm:
-              glVertexArrayAttribFormat(msh.vao, Gluint norm, 3, cGlFloat, GlFalse, 0)
-            of uv:
-              glVertexArrayAttribFormat(msh.vao, Gluint uv, 3, cGlFloat, GlFalse, 0)
-            of col:
-              glVertexArrayAttribFormat(msh.vao, Gluint col, 4, cGlFloat, GlFalse, 0)
+      let mat = transform
+      for vertInd in 0..<mesh.vertexCount:
+        let
+          vert = cast[Vec3](mesh.vertices[vertInd]).vec4(1)
 
-        glVertexArrayElementBuffer(msh.vao, msh.indices)
+          tsfm = mat4(
+            vec4(mat[0], mat[4], mat[8], mat[12]),
+            vec4(mat[1], mat[5], mat[9], mat[13]),
+            vec4(mat[2], mat[6], mat[10], mat[14]),
+            vec4(mat[3], mat[7], mat[11], mat[15])
+          )
 
-        result.buffers.add msh
+        mesh.vertices[vertInd] = cast[TVector3D]((tsfm * vert).xyz)
+
+      glCreateBuffers(ord(VboKinds.high) + 1, vbos[vert].addr)
+      glNamedBufferStorage(vbos[vert], mesh.vertexCount * vboSize[vert], mesh.vertices, GLbitfield 0)
+
+      if norm in components:
+        glNamedBufferStorage(vbos[norm], mesh.vertexCount * vboSize[norm], mesh.normals, GLbitfield 0)
+
+      if uv in components:
+        glNamedBufferStorage(vbos[uv], mesh.vertexCount * vboSize[uv], mesh.texCoords[0], GLbitfield 0)
+
+      if col in components:
+        glNamedBufferStorage(vbos[col], mesh.vertexCount * vboSize[col], mesh.colors[0], GLbitfield 0)
+
+      var msh: Mesh
+      glCreateVertexArrays(1, msh.vao.addr)
+      glCreateBuffers(1, msh.indices.addr)
+
+      var
+        indices = newSeqOfCap[cint](mesh.faceCount * 3)
+
+      for face in mesh.ifaces:
+        if face.indexCount != 3:
+          raise (ref ValueError)(msg: "Only acceepting triangulated meshes")
+        indices.add cast[ptr array[3, cint]](face.indices)[]
+
+      msh.size = indices.len.GlSizei
+
+      glNamedBufferStorage(msh.indices,
+        msh.size * sizeof(cint),
+        indices[0].addr,
+        GLbitfield 0
+      )
+
+      glVertexArrayElementBuffer(msh.vao, msh.indices)
+
+      for ind, vbo in vbos.pairs:
+        if ind in components:
+          glVertexArrayVertexBuffer(msh.vao, Gluint ind, vbo, 0, vboSize[ind])
+          glEnableVertexArrayAttrib(msh.vao, Gluint ind)
+          case ind
+          of vert:
+            glVertexArrayAttribFormat(msh.vao, Gluint vert, 3, cGlFloat, GlFalse, 0)
+          of norm:
+            glVertexArrayAttribFormat(msh.vao, Gluint norm, 3, cGlFloat, GlFalse, 0)
+          of uv:
+            glVertexArrayAttribFormat(msh.vao, Gluint uv, 3, cGlFloat, GlFalse, 0)
+          of col:
+            glVertexArrayAttribFormat(msh.vao, Gluint col, 4, cGlFloat, GlFalse, 0)
+
+      glVertexArrayElementBuffer(msh.vao, msh.indices)
+
+      result.buffers.add msh
     aiReleaseImport(scene)
 
 proc uploadData*(mesh: MeshData): Model =
