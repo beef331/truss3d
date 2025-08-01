cdaa1c6a2c8ad50e21bfdb7207d649c488f6f228
Jason Beetham <beefers331@gmail.com>
2025-08-01 15:14:56 -0600

    Changed materials to be more usable by allow ref types and normal types

--- src/truss3D/materials.nim
+++ src/truss3D/materials.nim
@@ -5,44 +5,69 @@ import std/tables
 const bufferCount = 32
 
 
+template property(T): typedesc = T | (ref T) | ptr T
+
 type
-  PropertyType = union(float32 | int32 | uint32 | Vec2 | IVec2 | UVec2 | Vec3 | IVec3 | UVec3 | Vec4 | IVec4 | UVec4 | Mat2 | Mat3 | Mat4 | ref Texture | Texture | ptr Texture)
+  Property =
+    property(float32) |
+    property(int32) |
+    property(uint32) |
+    property(Vec2) |
+    property(IVec2) |
+    property(UVec2) |
+    property(Vec3) |
+    property(IVec3) |
+    property(UVec3) |
+    property(Vec4) |
+    property(IVec4) |
+    property(UVec4) |
+    property(Mat2) |
+    property(Mat3) |
+    property(Mat4) |
+    property(Texture) |
+    property(TextureArray)
+
+  PropertyType = union(Property)
 
   MaterialProperty = object
     uniformName: string
     data: PropertyType
+    required: bool
 
   Material* = object
     uniforms: Table[string, MaterialProperty]
-    buffers: array[32, Gluint]
+    buffers: array[bufferCount, Gluint]
+    usedBuffers: set[0 .. bufferCount - 1]
     shader*: ref Shader
 
-proc setProperty*[T](material: var Material, name: string, val: sink T) =
+proc setProperty*[T: Property](material: var Material, name: string, val: sink T, required = true) =
   material.uniforms[name] = MaterialProperty(
     uniformName: name,
-    data: ensureMove(val) as PropertyType
+    data: ensureMove(val) as PropertyType,
+    required: required
   )
 
-proc setUniform*(shader: Shader, name: string, tex: ref Texture | ptr Texture) =
-  shader.setUniform(name, tex[])
+proc setUniform*(shader: Shader, name: string, tex: ref | ptr, required: bool) =
+  shader.setUniform(name, tex[], required)
 
 proc setUniform*(shader: Shader, prop: MaterialProperty) =
-  for name, field in prop.fieldPairs:
-    when name notin ["uniformName", "kind"]:
-      field.unpack:
-        shader.setUniform(prop.uniformName, it)
-        return
+  prop.data.unpack(it):
+    shader.setUniform(prop.uniformName, it, prop.required)
 
-proc setBuffer*[T](mat: var Material, binding: 0..31, buffer: Ssbo[T]) =
+proc setBuffer*[T](mat: var Material, binding: 0 .. bufferCount - 1, buffer: Ssbo[T]) =
+  if buffer.Gluint == 0:
+    mat.usedBuffers.excl binding
+  else:
+    mat.usedBuffers.incl binding
   mat.buffers[binding] = buffer.Gluint
-
 
 template with*(material: Material, body: untyped) =
   with material.shader[]:
     for name, uniform in material.uniforms.pairs:
       assert name == uniform.uniformName # Dumb but assurance is assurance
       material.shader[].setUniform(uniform)
-    for i, buffer in material.buffers.pairs:
-      if buffer != 0:
-        glBindBufferBase(GlShaderStorageBuffer, Gluint(i), buffer)
+    for ind in material.usedBuffers:
+      glBindBufferBase(GlShaderStorageBuffer, Gluint(ind), material.buffers[ind])
     body
+
+
